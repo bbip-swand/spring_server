5768845a1d93f9ddd89732d29f21682602dafc5f
singsangssong <plmko0914@gmail.com>
2025-05-01 20:51:19 +0900

    first commit

---
+++ src/main/java/kr/bbip/bbip/BbipApplication.java
@@ -0,0 +1,13 @@
+package kr.bbip.bbip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/bbip/bbip/BbipApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.bbip.bbip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
